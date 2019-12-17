cc8de59787cc81aa71a4db3586f758828f1395bf
Mohammed Ahmed Abd Al-Fattah <mohamedstarwars@gmail.com>
2019-12-17 20:08:45 +0200

    env.c: Adding comment

--- env.c
+++ env.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Getting the path environment variable*/
+
 void main(void)
-	{
+{
 	char * envVarName;
 	char * path;
 	envVarName = "PWD";
 	path = getenv(envVarName);
 	printf("Path is %s\n",path);
-
-	}
+}
